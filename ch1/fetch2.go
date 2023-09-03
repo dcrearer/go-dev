fd4d70889f231bb951263b6eca43fd685a6e00c2
Crearer <crearerd@a483e79e7d10.ant.amazon.com>
2023-09-03 16:43:38 -0400

    working through ch1

---
+++ ch1/fetch2.go
@@ -0,0 +1,25 @@
+// Fetch prints the content found at a URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		io.Copy(os.Stdout, resp.Body)
+		fmt.Fprintf(os.Stdout, "\nHTTP Status code: %s\n", resp.Status)
+	}
+}
